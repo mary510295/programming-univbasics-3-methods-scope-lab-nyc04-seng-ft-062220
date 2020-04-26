1662767cded9535e9e7ba0c7c9feb81d4d1cc924
Fu <fu.goto@gmail.com>
2020-04-26 16:50:21 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,5 @@
 
-status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 def mario
   phrase = "It's-a me, Mario!"
@@ -7,7 +7,7 @@ def mario
 end
 
 def toadstool
-  puts status
+  puts $status
 end
 
 puts status
